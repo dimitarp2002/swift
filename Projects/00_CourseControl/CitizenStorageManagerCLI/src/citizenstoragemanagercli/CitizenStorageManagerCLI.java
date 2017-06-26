868e765f7093b4f324d85719a4a074fdc02a709c
dimitar <dimitar@DimitarSF310>
2017-06-26 10:35:10 +0300

    working instance

--- Projects/00_CourseControl/CitizenStorageManagerCLI/src/citizenstoragemanagercli/CitizenStorageManagerCLI.java
+++ Projects/00_CourseControl/CitizenStorageManagerCLI/src/citizenstoragemanagercli/CitizenStorageManagerCLI.java
@@ -1,43 +1,52 @@
 package citizenstoragemanagercli;
 
-import education.Education;
+import address.Address;
 import insurance.SocialInsuranceRecord;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import personaldetails.Citizen;
-import storages.CitizenStorage;
-import storages.DALException;
-import storages.EducationStorage;
-import storages.MySqlCitizenStorage;
-import storages.MySqlEducationStorage;
-import storages.MySqlSocialInsuranceStorage;
-import storages.SocialInsuranceStorage;
+import personaldetails.Gender;
+import storages.*;
+import static storages.MySqlEducationStorage.createEducation;
 
 public class CitizenStorageManagerCLI {
 
     public static void main(String[] args) throws DALException {
-        
-        String url = "jdbc:mysql://localhost:3306/Citizen_and_SI_db?useSSL=false";
+
+        String url = "jdbc:mysql://localhost:3306/Citizen_and_SI_db?useSSL=false&useUnicode=true&characterEncoding=UTF-8";
         String username = "dimitar";
         String password = "123456";
 
-        CitizenStorage storage
-                = new MySqlCitizenStorage(url, username, password);
-        EducationStorage storageE
-                = new MySqlEducationStorage(url, username, password);
-        SocialInsuranceStorage storageS
-                = new MySqlSocialInsuranceStorage(url, username, password);
+        CitizenStorage storageCitizen;
+        AddressStorage storageAddress;
+        EducationStorage storageEducation;
+        SocialInsuranceStorage storageSocialInsurance;
 
-        //CLEAR THE TABLES
-        
+        try {
+            storageCitizen = new MySqlCitizenStorage(url, username, password);
+            storageAddress = new MySqlAddressStorage(url, username, password);
+            storageEducation = new MySqlEducationStorage(url, username, password);
+            storageSocialInsurance = new MySqlSocialInsuranceStorage(url, username, password);
+
+        } catch (SQLException ex) {
+            throw new DALException("Unable to open storage", ex);
+        }
+
+        storageEducation.truncateEducationTable();
+        storageCitizen.truncateCitizenTable();
+        storageAddress.truncateAddressTable();
+        storageSocialInsurance.truncateSocialInsTable();
+
         String filename;
         Scanner sc;
+        List<Citizen> citizens = new ArrayList<>();
+        int citizenId = 0;
 
         if (args.length > 0) {
             filename = args[0];
@@ -46,102 +55,123 @@ public class CitizenStorageManagerCLI {
                 sc = new Scanner(new FileReader(filename));
 
             } catch (FileNotFoundException ex) {
-                System.out.println("File not found.");
-                return;
+                throw new DALException("File not found", ex);
             }
+
         } else {
             sc = new Scanner(System.in);
         }
-        DateTimeFormatter formatter
-                = DateTimeFormatter.ofPattern("d.M.yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.yyyy");
         int count = sc.nextInt();
         sc.nextLine();
-        for (int i = 0; i < count * 2; i++) {
+
+        for (int i = 0; i < count; i++) {
             String line = sc.nextLine();
-            if (i % 2 == 0) {
-
-                String[] split = line.split(";");
-//                for (String s : split) {
-//                    System.out.println(s);
-//                }
-                String firstName = split[0].trim();
-                String middleName = split[1].trim();
-                String lastName = split[2].trim();
-                String genderStr = split[3].trim();
-                LocalDate dateOfBirth = LocalDate.parse(split[4].trim(), formatter);
-                int height = Integer.parseInt(split[5].trim());
-                String country = split[6].trim();
-                String city = split[7].trim();
-                String municipality = split[8].trim();
-                String postalCode = split[9].trim();
-                String street = split[10].trim();
-                String streetNumber = split[11].trim();
-
-                Integer floor = null;
-                try {
-                    if (split.length > 12) {
-                        floor = Integer.parseInt(split[12].trim());
-                    }
-                } catch (NumberFormatException ex) {
-                    floor = null;
-                }
-
-                Integer appNo = null;
-                try {
-                    if (split.length > 13) {
-                        appNo = Integer.parseInt(split[13].trim());
-                    }
-                } catch (NumberFormatException ex) {
-                    appNo = null;
-                }
-
-                int j =0;
-                while (14+j<split.length) {
-
-                    String educationType = split[14+(j++)].trim();
-                    String institution_name = split[14+(j++)].trim();
-                    LocalDate enrollmentDate = LocalDate.parse(split[14+(j++)].trim(), formatter);
-                    LocalDate graduationDate = LocalDate.parse(split[14+(j++)].trim(), formatter);
-                    Float grade= null;
-                    switch (educationType) {
-                        case "S" : grade = Float.parseFloat(split[14+(j++)].trim());
-                        break;
-                        case "M" : grade = Float.parseFloat(split[14+j++].trim());
-                        break;
-                        case "B" : grade = Float.parseFloat(split[14+j++].trim());
-                        break;
-                        case "D" : grade = Float.parseFloat(split[14+j++].trim());
-                        case "P" : 
-                    }
-
-                    System.out.printf("%s %s %s %s %s %n", 
-                            educationType, institution_name, enrollmentDate , graduationDate,  grade);
-                }
-                System.out.printf("%s %s %s %s  %s %d %s %s %s %s %s %s %d %d %n%n",
-                        firstName, middleName, lastName, genderStr, dateOfBirth, height, country, city, municipality, postalCode,
-                        street, streetNumber, floor, appNo);
+            String[] split = line.split(";");
+            String firstName = split[0].trim();
+            String middleName = split[1].trim();
+            String lastName = split[2].trim();
+            Gender gender;
+            if (split[3].trim().equalsIgnoreCase("M")) {
+                gender = Gender.Male;
+            } else {
+                gender = Gender.Female;
             }
+            LocalDate dateOfBirth = LocalDate.parse(split[4].trim(), formatter);
+            int height = Integer.parseInt(split[5].trim());
+            Citizen citizen = new Citizen(firstName, middleName, lastName, gender, height, dateOfBirth);
+
+            String country = split[6].trim();
+            String city = split[7].trim();
+            String municipality = split[8].trim();
+            String postalCode = split[9].trim();
+            String street = split[10].trim();
+            String streetNumber = split[11].trim();
+
+            Integer floor = null;
+            try {
+                if (split.length > 12) {
+                    floor = Integer.parseInt(split[12].trim());
+                }
+            } catch (NumberFormatException ex) {
+                floor = null;
+            }
+
+            Integer appNo = null;
+            try {
+                if (split.length > 13) {
+                    appNo = Integer.parseInt(split[13].trim());
+                }
+            } catch (NumberFormatException ex) {
+                appNo = null;
+            }
+            citizen.setAddress(new Address(country, city, municipality, postalCode, street, streetNumber, floor, appNo));
+
+            int j = 0;
+            while (14 + j < split.length) {
+
+                String degree = split[14 + (j++)].trim();
+                String institutionName = split[14 + (j++)].trim();
+                LocalDate enrollmentDate = LocalDate.parse(split[14 + (j++)].trim(), formatter);
+                LocalDate graduationDate = LocalDate.parse(split[14 + (j++)].trim(), formatter);
+
+                Float grade = null;
+                try {
+                    //check if there are more educations to be added:
+                    if (14 + (j + 1) < split.length) {
+                        //check if the next field is type of education {P,S,B,M,D} or grade:
+                        if (split[14 + j].equalsIgnoreCase("P") || split[14 + j].equalsIgnoreCase("S")
+                                || split[14 + j].equalsIgnoreCase("B") || split[14 + j].equalsIgnoreCase("M")
+                                || split[14 + j].equalsIgnoreCase("D")) {
+                            // if 14+j is type of Education then grade remains NULL
+                            citizen.addEducation(createEducation(institutionName, enrollmentDate, graduationDate, degree, grade));//createEducation is defined in MySQLEducationStorage
+                        } else {
+                            // else 14+j is a grade and we will parse it:
+                            grade = Float.parseFloat(split[14 + (j++)].trim());
+                            citizen.addEducation(createEducation(institutionName, enrollmentDate, graduationDate, degree, grade));//createEducation is defined in MySQLEducationStorage
+                        }
+                    } else // there are no more education to be added, the current is the last
+                     if (14 + (j + 1) == split.length) {
+                            // grade exists and we will parse it 
+                            grade = Float.parseFloat(split[14 + (j++)].trim());
+                            citizen.addEducation(createEducation(institutionName, enrollmentDate, graduationDate, degree, grade)); //createEducation is defined in MySQLEducationStorage
+                            break;
+                        } else {
+                            // grade doen'r exist, so now grade remains NULL
+                            citizen.addEducation(createEducation(institutionName, enrollmentDate, graduationDate, degree, grade));//createEducation is defined in MySQLEducationStorage
+                            break;
+                        }
+
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    storageEducation.truncateEducationTable();
+                    storageCitizen.truncateCitizenTable();
+                    storageAddress.truncateAddressTable();
+                    storageSocialInsurance.truncateSocialInsTable();
+                    throw new DALException("Unsuccessful parsing of lines", ex);
+
+                }
+            }
+
+            line = sc.nextLine();
+            split = line.split(";");
+
+            List<SocialInsuranceRecord> siRecords = new ArrayList<>();
+            for (int k = 0; k < split.length;) {
+                int year = Integer.parseInt(split[k++].trim());
+                int month = Integer.parseInt(split[k++].trim());
+                double amount = Double.parseDouble(split[k++].trim());
+                citizen.addSocialInsuranceRecord(new SocialInsuranceRecord(year, month, amount));
+
+            }
+            citizens.add(citizen);
         }
 
-        
-        
-        
+        storageCitizen.insert(citizens);
+        for (int i = 0; i < citizens.size(); i++) {
+            storageAddress.insert(citizens.get(i).getAddress(), i + 1);
+            storageEducation.insert(citizens.get(i).getEducations(), i + 1);
+            storageSocialInsurance.insert(citizens.get(i).getSocialInsuranceRecords(), i + 1);
 
-        Citizen person = storage.getCitizenById(1);
-        person.printPerson();
-
-        List<Education> educations = storageE.getEducationById(1);
-        for (Education education : educations) {
-            person.addEducation(education);
         }
-        person.printEducations();
-
-        List<SocialInsuranceRecord> siRecords = storageS.getSocialInsuranceById(1);
-        for (SocialInsuranceRecord siRecord : siRecords) {
-            person.addSocialInsuranceRecord(siRecord);
-        }
-        person.printSrRecords();
-
     }
-
 }
